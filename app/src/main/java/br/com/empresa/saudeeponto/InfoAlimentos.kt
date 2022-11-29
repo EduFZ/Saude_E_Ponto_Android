7ffc82e7f92601b475f9176ae2a483d0857d8f60
Eduardo F Zimmermann <edu.zimmermann.efz@gamil.com>
2022-11-28 21:03:15 -0300

    Iniciado construção da activity info_alimentos

---
+++ app/src/main/java/br/com/empresa/saudeeponto/InfoAlimentos.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.saudeeponto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoAlimentos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_alimentos)
+    }
+}
